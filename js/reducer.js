ef174b0b81d253de3055a42afb20d11bab2d696c
jonathanboss17 <jtboss47@gmail.com>
2019-12-16 11:15:31 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,25 +1,29 @@
 // add code snippets from README
 let state; 
-let button = document.getElementById('button'); 
+const button = document.getElementById('button'); 
 
-function reducer(state = {count: 0}, action){
-    switch(action.type) {
+const reducer = (state = {count: 0}, action) => {
+    switch (action.type) {
         case 'INCREASE_COUNT': 
-            return {count: state.count + 1}; 
+            return {count: state.count + 1}
         default: 
             return state; 
     }
 }
 
-function dispatch(action){
+const dispatch = (action) => {
     state = reducer(state, action); 
     render(); 
 }
 
-function render(){
+const render = () => {
     let container = document.getElementById('container'); 
     container.textContent = state.count; 
 }
+
+// displays the default state on the page ...
+// '@@INIT' can be any string
+dispatch({type: '@@INIT'})
 
 button.addEventListener('click', () => {
     dispatch({type: 'INCREASE_COUNT'})
